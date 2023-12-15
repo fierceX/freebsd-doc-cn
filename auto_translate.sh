110af2394f66ea4550daab5b7a06abd82a87974c
fiercex <fiercex@outlook.com>
2023-12-15 17:06:31 +0800

    upup

--- auto_translate.sh
+++ auto_translate.sh
@@ -1,10 +1,9 @@
 NEW_PATH="zh-cn/books/handbook"
 
 
-for document in $NEW_PATH/*; do
+for document in $(find "$NEW_PATH" -name "*.po" ); do
 
 	echo $document
 	python3 auto_translate.py --input $document --output $document
-    set
 
 done	
