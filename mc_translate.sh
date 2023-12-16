05efadc34ce4fcc20c529cb6caa372889f4ba0ef
fiercex <fiercex@outlook.com>
2023-12-16 22:09:11 +0800

    up

--- mc_translate.sh
+++ mc_translate.sh
@@ -6,6 +6,12 @@ NEW_PATH="zh-cn/books/handbook"
 for document in $(find "$NEW_PATH" -name "*.po" ); do
 
 	echo $document
+	name=$(basename "$document")
 	python3 mc_translate.py --input $document --output $document --type chatgpt
+	set +x
+	git add zh-cn
+    git commit -m "mc_translate_$name"
+	git push
+	set -x
 
 done	
